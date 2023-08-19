e704b049912092d0b4bbddb1962e91e991719407
redstart <rslnkssk@gmail.com>
2023-08-19 08:36:34 +0200

    define hash map

---
+++ core/hash_map.c
@@ -0,0 +1,34 @@
+//
+// Created by redstart on 8/19/23.
+//
+
+#include "hash_map.h"
+
+short hash(uint64_t id) {
+  // This function ensures that hashCodes that differ only by
+  // constant multiples at each bit position have a bounded
+  // number of collisions (approximately 8 at default load factor).
+  id ^= (id >> 20) ^ (id >> 12);
+  return (short)((id ^ (id >> 7) ^ (id >> 4)) % DEFAULT_BUFFER);
+}
+
+hash_map *hash_map__new(uint64_t key, void *value) {
+  hash_map *map = calloc(1, sizeof(*map));
+  if (!map)
+    throw_error("Error! Cannot create map");
+  short index = hash(key);
+  map->map[index].value = value;
+  return map;
+}
+
+void hash_map__insert(hash_map *self, uint64_t key, void *value) {
+  self->map[hash(key)].value = value;
+}
+
+void *hash_map__get(hash_map *self, uint64_t key) {
+  return self->map[hash(key)].value;
+}
+
+void hash_map__drop(hash_map *self) {
+  delete(self);
+}
