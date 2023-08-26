5a40344238751c3ff55c42a3fae885c14645228f
redstart <rslnkssk@gmail.com>
2023-08-26 15:43:33 +0200

    add parsingError to eliminate ifs

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -8,7 +8,7 @@
 #include "toml.hpp"
 #include <SDL2/SDL.h>
 #include <functional>
-
+void parsingError(const std::string &msg, const std::string &path);
 void insertVectorToMap(
     const std::string &componentTypeStr, IComponent *newComponent,
     std::unordered_map<std::string, std::vector<IComponent *>> &components);
@@ -27,34 +27,33 @@ Engine::~Engine() {
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(screen);
   for (auto &entity: entities) { delete entity.second; }
-  for (auto &i: components)
-    for (auto &component: i.second) delete component;
+  for (auto &componentType: components)
+    for (auto &component: componentType.second) delete component;
   SDL_Quit();
 }
 
 void Engine::loadScenes() {
   auto data = toml::parse(resources + "main.toml");
   const auto &scenesArray = data["scenes"].as_array();
-  if (scenesArray.empty()) {
-    Logger::error("No scenes. Cannot load project. Exiting now.");
-    throw std::runtime_error("Check " + resources + " folder for main.toml");
-  }
-  for (const auto &scene: scenesArray) { scenes.push_back(scene.as_string()); }
+  if (scenesArray.empty())
+    parsingError("No scenes. Cannot load project. Exiting now.", resources);
+  for (const auto &scene: scenesArray) scenes.push_back(scene.as_string());
+}
+
+void parsingError(const std::string &msg, const std::string &path) {
+  Logger::error(msg);
+  throw std::runtime_error("Check " + path);
 }
 
 void Engine::readScene(int index) {
   std::string path = resources + scenes[index];
   auto data = toml::parse(path + "/blueprint.toml");
   Logger::info("Trying to read " + path);
-  if (data.is_uninitialized() || data["entities"].is_uninitialized()) {
-    Logger::error("Bad blueprint. Cannot load project. Exiting now.");
-    throw std::runtime_error("Check " + path + " folder for blueprint.toml");
-  }
+  if (data.is_uninitialized() || data["entities"].is_uninitialized())
+    parsingError("Bad blueprint. Cannot load project. Exiting now.", path);
   const auto &entitiesArray = data["entities"].as_array();
-  if (entitiesArray.empty()) {
-    Logger::error("No entities. Cannot load project. Exiting now.");
-    throw std::runtime_error("Check " + path + "/blueprint.toml");
-  }
+  if (entitiesArray.empty())
+    parsingError("No entities. Cannot load project. Exiting now.", path);
   for (const auto &entity: entitiesArray) {
     std::string entity_name = entity.as_string();
     std::string entity_path = path;
@@ -94,11 +93,7 @@ void Engine::run() {
     SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderClear(renderer);
     // [WIP] delta time should be passed to update instead of magic number
-    systems.update({
-                        components,
-                        entities,
-                        renderer
-                    }, 1);
+    systems.update({components, entities, renderer}, 1);
     SDL_RenderPresent(renderer);
     usleep(15000);
   }
